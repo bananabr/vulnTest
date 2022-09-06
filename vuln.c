b05407b2ed65201b4ba1a3108925f37321c23146
Daniel Santos <bananabr@users.noreply.github.com>
2022-09-06 17:38:17 -0500

    Create vuln.c

---
+++ vuln.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char **argv){
+        if(argc < 3){
+                return 1;
+        }
+        int n = atoi(argv[1]);
+        char buff[256];
+        memset(buff, 0, sizeof(buff));
+        strncpy(buff, argv[2], n);
+        printf("%s\n",buff);
+}
